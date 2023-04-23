2a8f200e54ba47eabd36f03477f4d034376de50f
yealem <tyealem@gmail.com>
2023-04-23 07:22:29 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
