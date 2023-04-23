1d8f8d7ef87cde2e4a2b97577fe85e4a2f552cbb
yealem <tyealem@gmail.com>
2023-04-23 14:51:24 -0700

    Positive or negative

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	char 1;
+	int 4;
+	long int 4;
+	long long int 8;
+	float 4;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(1));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(4));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(4));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(8));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(4));
 return (0);
 }
---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: 'Check for number if it is positive or negative'
+ * Return: always o
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
